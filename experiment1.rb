6df51e2bfdcb2872b325aad287829f288029c776
mac2 <mac2@example.com>
2019-04-10 13:37:56 +0900

    add

--- experiment1.rb
+++ experiment1.rb
@@ -6,25 +6,33 @@ require 'tmpdir'
 
 def concat_audio(input_paths, output_path)
   File.open(output_path, 'wb') do |output|
-    input_paths.each_slice(16).each do |slice|
-      input_data = Parallel.map(slice, in_threads: 8) do |input_path|
-        warn input_path
+    input_paths.each_slice(32).each do |slice|
+      input_data = Parallel.map(slice, in_threads: 16) do |input_path|
+        Dir.mktmpdir do |dir|
+          warn input_path
 
-        case input_path
-        when /\.(txt|jpg|jpeg|png|json)/
-          nil
-        when /\.raw$/
-          File.read(input_path)
-        else
-          Dir.mktmpdir do |dir|
-            tmp_path = "#{dir}/output.raw"
-            ffmpeg_output = `ffmpeg -i #{input_path} -ac 1 -ar 48000 -f s16le -acodec pcm_s16le #{tmp_path} 2>&1`
-            if File.exist?(tmp_path) && File.size(tmp_path) > 0
-              File.read(tmp_path)
-            else
+          case input_path
+          when /\.(txt|jpg|jpeg|png|json)/
+            next nil
+          when /\.raw$/
+            pcm_path = input_path
+          else
+            pcm_path = "#{dir}/output.raw"
+            ffmpeg_output = `ffmpeg -i #{input_path} -ac 1 -ar 48000 -f s16le -acodec pcm_s16le #{pcm_path} 2>&1`
+            unless File.exist?(pcm_path) && File.size(pcm_path).positive?
               warn "failed #{input_path} #{ffmpeg_output}"
-              nil
+              next nil
             end
+          end
+
+          normalized_path = "#{dir}/normalized.raw"
+          sox_output = `sox -r 48000 -e signed -c 1 -b 16 #{pcm_path} #{normalized_path} gain -n 2>&1`
+
+          if File.exist?(normalized_path) && File.size(normalized_path).positive?
+            File.read(normalized_path)
+          else
+            warn "failed #{input_path} #{sox_output}"
+            next nil
           end
         end
       end
@@ -48,7 +56,7 @@ class MyCLI < Thor
   option :input, required: true, desc: 'dir path containing clean audio'
   option :output, required: true, desc: 'output raw pcm path'
   def prepare_pcm
-    concat_audio(Dir.glob("#{options[:input]}/*.*").sort, options[:output])
+    concat_audio(Dir.glob("#{options[:input]}/**/*.*").sort, options[:output])
   end
 
   desc 'prepare_vec', 'prepare '
@@ -61,20 +69,27 @@ class MyCLI < Thor
   desc 'manual', 'show manual'
   def manual
     warn <<EOS
-# to get noise data
+# get noise data
 curl -L https://people.xiph.org/~jm/demo/rnnoise/rnnoise_contributions.tar.gz | tar zx -C /tmp
+
+# get speech data
+curl -L http://www-mmsp.ece.mcgill.ca/Documents/Data/TSP-Speech-Database/48k.zip > /tmp/48k.zip
+unzip -d /tmp /tmp/48k.zip
 
 # setup
 bundle install
 pipenv install
 
 # prepare training data
-bundle exec ruby experiment1 prepare_pcm --input /path/to/input --output clean.raw
-bundle exec ruby experiment1 prepare_pcm --input /tmp/rnnoise_contributions --output noise.raw
-bundle exec ruby experiment1 prepare_vec --output-count 50000
+bundle exec ruby experiment1.rb prepare_pcm --input /tmp/48k --output clean.raw
+bundle exec ruby experiment1.rb prepare_pcm --input /tmp/rnnoise_contributions --output noise.raw
+bundle exec ruby experiment1.rb prepare_vec --output-count 50000
 
 # train
 pipenv run python training/rnn_train.py
+
+# use gpu in docker container
+
 EOS
   end
 end
