d19a03e4e558f3d202f16054b33288fb419e9a0f
mac2 <mac2@example.com>
2019-04-14 19:20:14 +0900

    add cnn

--- training/rnn_train.py
+++ training/rnn_train.py
@@ -18,6 +18,9 @@ from keras.models import Model
 from keras.layers import Input
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Conv2D
+from keras.layers import Flatten
+from keras.layers import Reshape
 from keras.layers import BatchNormalization
 from keras.layers import CuDNNGRU
 from keras.layers import GRU
@@ -56,6 +59,8 @@ parser.add_argument('--learning_rate', type=float, default=1e-3)
 parser.add_argument('--safe_crossentropy_factor', type=float, default=0.0)
 parser.add_argument('--loss_type', default='original')
 parser.add_argument('--log_loss_bias', type=float, default='1e-7')
+parser.add_argument('--arch', default='original')
+parser.add_argument('--window_size', type=int, default=2000)
 args = parser.parse_args()
 
 def my_safecrossentropy(y_pred, y_true):
@@ -169,32 +174,48 @@ print(args)
 
 # gru_lrelu_alpha = 1.0 / 5.5 # from "Empirical Evaluation of Rectified Activations in Convolution Network"
 
-main_input = Input(shape=(None, 42), name='main_input')
-tmp = Dense(math.ceil(24 * args.hidden_units), activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
-if args.dropout > 0:
-    tmp = Dropout(args.dropout)(tmp)
+window_size = args.window_size
 
-vad_gru = create_gru(math.ceil(24 * args.hidden_units), 'vad_gru')(tmp)
-if args.dropout > 0:
-    vad_gru = Dropout(args.dropout)(vad_gru)
-# vad_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="vad_gru_activation")(GRU(24, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(tmp))
-vad_output = Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(vad_gru)
+if args.arch == 'original':
+    main_input = Input(shape=(None, 42), name='main_input')
 
-noise_input = keras.layers.concatenate([tmp, vad_gru, main_input])
-noise_gru = create_gru(math.ceil(48 * args.hidden_units), 'noise_gru')(noise_input)
-if args.dropout > 0:
-    noise_gru = Dropout(args.dropout)(noise_gru)
-# noise_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="noise_gru_activation")(GRU(48, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='noise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(noise_input))
+    tmp = Dense(math.ceil(24 * args.hidden_units), activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
+    if args.dropout > 0:
+        tmp = Dropout(args.dropout)(tmp)
 
-denoise_input = keras.layers.concatenate([vad_gru, noise_gru, main_input])
-denoise_gru = create_gru(math.ceil(96 * args.hidden_units), 'denoise_gru')(denoise_input)
-if args.dropout > 0:
-    denoise_gru = Dropout(args.dropout)(denoise_gru)
-# denoise_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="denoise_gru_activation")(GRU(96, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='denoise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(denoise_input))
+    vad_gru = create_gru(math.ceil(24 * args.hidden_units), 'vad_gru')(tmp)
+    if args.dropout > 0:
+        vad_gru = Dropout(args.dropout)(vad_gru)
+    # vad_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="vad_gru_activation")(GRU(24, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(tmp))
+    vad_output = Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(vad_gru)
 
-denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
+    noise_input = keras.layers.concatenate([tmp, vad_gru, main_input])
+    noise_gru = create_gru(math.ceil(48 * args.hidden_units), 'noise_gru')(noise_input)
+    if args.dropout > 0:
+        noise_gru = Dropout(args.dropout)(noise_gru)
+    # noise_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="noise_gru_activation")(GRU(48, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='noise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(noise_input))
 
-model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
+    denoise_input = keras.layers.concatenate([vad_gru, noise_gru, main_input])
+    denoise_gru = create_gru(math.ceil(96 * args.hidden_units), 'denoise_gru')(denoise_input)
+    if args.dropout > 0:
+        denoise_gru = Dropout(args.dropout)(denoise_gru)
+    # denoise_gru = keras.layers.LeakyReLU(alpha=gru_lrelu_alpha, name="denoise_gru_activation")(GRU(96, activation=None, recurrent_activation='sigmoid', return_sequences=True, name='denoise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(denoise_input))
+
+    denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
+
+    model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
+elif args.arch == 'cnn':
+    main_input = Input(shape=(window_size, 42), name='main_input')
+    reshaped = Reshape((window_size, 42, 1))(main_input)
+    conv1 = Conv2D(8, (3, 3), dilation_rate=(2, 2), padding='same', activation='elu')(reshaped)
+    conv2 = Conv2D(8, (3, 3), dilation_rate=(2, 2), padding='same', activation='elu')(conv1)
+    conv3 = Conv2D(8, (3, 3), dilation_rate=(2, 2), padding='same', activation='elu')(conv2)
+    flatten = Flatten()(conv3)
+    vad_output = Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(flatten)
+    denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(flatten)
+    model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
+else:
+    raise 'unknown arch'
 
 optimizer = keras.optimizers.Adam(lr=args.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-4, amsgrad=True)
 
@@ -203,7 +224,6 @@ model.compile(loss=[mycost, my_crossentropy],
               optimizer=optimizer, loss_weights=[10, 0.5])
 
 batch_size = args.batch_size
-window_size = 2000
 
 if args.mmap:
     print('Mmap .f32 file')
@@ -222,13 +242,13 @@ x_train = all_data[:nb_sequences*window_size, :42]
 x_train = np.reshape(x_train, (nb_sequences, window_size, 42))
 
 y_train = all_data[:nb_sequences*window_size, 42:64]
-y_train = np.reshape(y_train, (nb_sequences, window_size, 22))
+y_train = np.reshape(y_train, (nb_sequences, window_size, 22))[:,window_size // 2,:]
 
 noise_train = all_data[:nb_sequences*window_size, 64:86]
 noise_train = np.reshape(noise_train, (nb_sequences, window_size, 22))
 
 vad_train = all_data[:nb_sequences*window_size, 86:87]
-vad_train = np.reshape(vad_train, (nb_sequences, window_size, 1))
+vad_train = np.reshape(vad_train, (nb_sequences, window_size, 1))[:,window_size // 2,:]
 
 all_data = 0
 print(len(x_train), 'train sequences. x shape =', x_train.shape, 'y shape = ', y_train.shape)
